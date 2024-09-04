cdb6c5083053ee11e7b798e30f756346bd06e36c
xicilion <lion@9465.net>
2024-09-04 17:57:35 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,60 @@
+const events = require('eventemitter3');
+
+const disconnect_states = {
+    closed: true,
+    failed: true,
+    disconnected: true
+};
+
+module.exports = class Peer extends events.EventEmitter {
+    constructor(options) {
+        super();
+
+        this.connection = options.connection;
+        this.channel = options.channel;
+
+        let closed = false;
+        let This = this;
+
+        function close() {
+            if (closed) return;
+
+            closed = true;
+            This.emit('close');
+
+            This.connection.close();
+        }
+
+        this.connection.addEventListener("connectionstatechange", function () {
+            if (This.connection.connectionState in disconnect_states)
+                close();
+        });
+
+        this.channel.addEventListener("close", close);
+        this.channel.addEventListener("error", close);
+
+        this.channel.addEventListener("open", function () {
+            This.emit('open');
+        });
+
+        this.channel.addEventListener("message", function (ev) {
+            This.emit('message', ev);
+        });
+    }
+
+    addEventListener(event, listener) {
+        this.addListener(event, listener);
+    }
+
+    removeEventListener(event, listener) {
+        this.removeListener(event, listener);
+    }
+
+    send(data) {
+        this.channel.send(data);
+    }
+
+    close() {
+        this.connection.close();
+    }
+}
